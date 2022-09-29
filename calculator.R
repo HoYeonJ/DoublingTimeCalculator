b87c7258acf1d78ed517a494625671381f89c0f7
Ho Yeon <25444008+HoYeonJ@users.noreply.github.com>
2022-09-29 03:50:29 -0400

    Update calculator.R

--- calculator.R
+++ calculator.R
@@ -5,7 +5,8 @@ col = ncol(cellGrowth)
 row = nrow(cellGrowth)
 times = cellGrowth[,(1:1)]
 
-# Log transform the growth columns
+# Log transform the growth columns NOTE: ITS NOT ACTUALLY LOG TRANSFORMED RIGHT 
+# NOW
 #cellGrowthLog = log(cellGrowth[,(2:col)])
 cellGrowthLog = cellGrowth[,(2:col)]
 
@@ -25,26 +26,25 @@ for (y in (1:newcol)){
 }
 }
 
-# Find the max of each column
+# Find the max steepest slope of each column
 maxes = sapply(cellGrowthThirty[,1:newcol], which.max)
 print(maxes)
-# Take the 30 point window around the max
+# Take the 100 point window around the max to find the growth rate
 mat = as.data.frame(matrix(0, 3, newcol))
 arr = vector(mode = "double", length = newcol)
 timesarr = vector(mode = "double", length = newcol)
 for (y in (1:newcol)){
     center= maxes[y]
+    # Absorbance
     windows = cellGrowth[(center-50: center+50), y+1]
-    x = cellGrowth[(center-50: center+50),1]
+    # Time
+    x = times[(center-50: center+50)]
+    # Linear regression after log transformation
     model = lm(log(windows)~ x)
-    print(model$r.squared)
     timesarr[y] = times[center]
-    #mat[,y ]= c(log(2)/coefficients(model)[2], times[center], model$r.squared)
+    mat[,y ]= c(log(2)/coefficients(model)[2], times[center], 
+                summary(model)$r.squared)
 }
 print(mat)
-
-# Run linear regressions for each array
-
-# Find the doubling time
-
+# Result formatted as: Doubling time, center of steepest slope, R^2 value
 
